c21010da3efb56bde50029553b9741deb5b5fa65
Tomas Perez <tomasitopg6@gmail.com>
2020-08-08 05:35:19 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -12,7 +12,7 @@ class OwnersController < ApplicationController
 
   post '/owners' do
     @owner = Owner.create(params[:owner])
-  redirect "/owners/#{@owner.id}"
+    redirect "/owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do
